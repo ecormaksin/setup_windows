da383657616d6b7d50a29d28aade60e53d4256fc
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-02-18 22:00:27 +0900

    fix ghq-peco function

--- Profile.ps1
+++ Profile.ps1
@@ -2,22 +2,7 @@
     "`r`n`r`n$($executionContext.SessionState.Path.CurrentLocation)$(' ')`($(Get-Date -Format 'yyyy/MM/dd HH:mm:ss')`)`r`n$('>' * ($nestedPromptLevel + 1)) ";
 }
 
-Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardDeleteLine
-Set-PSReadlineKeyHandler -Key 'Ctrl+b' -Function BackwardChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+f' -Function ForwardChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+h' -Function BackwardDeleteChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
-Set-PSReadlineKeyHandler -Key 'Ctrl+a' -Function BeginningOfLine
-Set-PSReadlineKeyHandler -Key 'Ctrl+e' -Function EndOfLine
-
 function ghq-peco {
-    cd $(ghq list -p | peco)
-}
-
-Set-PSReadLineKeyHandler -Chord Ctrl+] -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("ghq-peco")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    $SelectedRepository = $(ghq list -p | peco)
+    Set-Location $SelectedRepository
 }
