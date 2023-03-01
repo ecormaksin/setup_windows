9e92996e1b8f43153cad85694e6f8df34c039cd7
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-03-01 15:55:31 +0900

    add sakuraeditor to choco

--- install_via_chocolatey.ps1
+++ install_via_chocolatey.ps1
@@ -1,4 +1,10 @@
 ﻿# install choclolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install devtoys
+$Apps = @(
+    'devtoys',
+    'SakuraEditor'
+)
+$Apps | ForEach-Object {
+    & choco install $_ --accept-license --confirm
+}
